2e996f4b5629371feb7c02fe758009f063b7948a
Shaunythunder <shaunythunder@gmail.com>
2025-04-07 22:23:35 -0400

    Completed CH2 L5: Added Player class and rendering

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from constants import * # Everything
+from player import Player
 
 def main():
 	
 	# Pygame start
 	pygame.init()
+	
 
 	# Variable definitions
 	clock = pygame.time.Clock()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	dt = 0 #Clock Timer
-	
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 	# Print starting messages
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,6 +22,7 @@ def main():
 	# Game loop start
 	while True:
 		screen.fill("black")
+		player.draw(screen)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
@@ -27,10 +31,8 @@ def main():
 		clock.tick(60)
 		# Keep this last
 		pygame.display.flip()
-		
-		
 
-	
+
 
 if __name__ == "__main__":
 	main()
